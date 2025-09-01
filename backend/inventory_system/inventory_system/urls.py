6c1c809f0ab5c1ca07ebb92054970b64a9330ab0
Nasam Odoi <bitam11-036@suza.ac.tz>
2025-09-01 21:16:33 +0300

    Ready for Render deployment with static files

--- backend/inventory_system/inventory_system/urls.py
+++ backend/inventory_system/inventory_system/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -27,5 +28,7 @@ urlpatterns = [
 
     # 2) Then your “everything else under /api/”:
     path('api/', include('inventory_app.urls')),
+     path('', TemplateView.as_view(template_name='index.html')),  # React entry point
+
 ]
 
